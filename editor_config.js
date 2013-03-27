587cec7f5636557ec4e638ba5e061b86c9d5ff93
许恒 <xuheng@baidu.com>
2013-03-27 10:53:19 +0800

    [fixed] by xuheng

--- editor_config.js
+++ editor_config.js
@@ -80,7 +80,7 @@
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         , toolbars:[
-            ['insertimage' ,'|','insertorderedlist','|', 'insertunorderedlist','|','autotypeset','|','insertmap']
+            ['insertimage' ,'|', 'insertunorderedlist','|','insertorderedlist','|','autotypeset','|','insertmap']
         ]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         ,labelMap:{
