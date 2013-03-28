6bedb564918a5cc438317f7f8d670a297dfdb236
许恒 <xuheng@baidu.com>
2013-03-28 15:12:43 +0800

    [fixed] by xuheng

--- _src/ui/_linkPicker.js
+++ _src/ui/_linkPicker.js
@@ -21,7 +21,8 @@
             arr.push('<div id="##" class="edui-linkpicker %%">' +
                 '<div class="edui-linkpicker-body">');
             for (var i = 0, item; item = list[i++];) {
-                arr.push('<div onclick="$$._onClick(event);" class="edui-linkpicker-item" stateful>' + item.title + '</div>');
+                arr.push('<div onclick="$$._onClick(event);" class="edui-linkpicker-item" stateful  _href="' +
+                    item.href + '" _title="' + item.title + '">' + item.title + '</div>');
             }
             arr.push('</div>' +
                 '<div onclick="$$._showDialog(event);" class="edui-linkpicker-edit edui-linkpicker-item">编辑</div>' +
@@ -37,6 +38,12 @@
             Popup.postHide(evt);
         },
         _onClick:function (evt) {
+            var target = evt.target || evt.srcElement, obj = {};
+            obj["href"] = target.getAttribute("_href");
+            obj["_href"] = target.getAttribute("_href");
+            obj["textValue"] = target.getAttribute("_title");
+            editor.execCommand('link', obj);
+            Popup.postHide(evt);
 
         },
         _UIBase_render:UIBase.prototype.render
