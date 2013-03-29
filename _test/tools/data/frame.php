239db0896fdf1c85001e7932220e9221d3cb0cbb
许恒 <xuheng@baidu.com>
2013-03-29 17:22:08 +0800

    [fixed] by xuheng

--- _test/tools/data/frame.php
+++ _test/tools/data/frame.php
@@ -8,7 +8,7 @@ $release = preg_match('/release=true/i', $_SERVER['QUERY_STRING']);
 if($release == 0 && array_key_exists('f', $_GET))
 print "<script type='text/javascript' src='../br/import.php?f={$_GET['f']}'></script>";
 else
-print "<script type='text/javascript' src='../../../release/all_release.js'></script>";
+print "<script type='text/javascript' src='../../../release-forqiyezhidao/all_release.js'></script>";
 ?>
 </head>
 <body>
