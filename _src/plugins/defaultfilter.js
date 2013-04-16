7a53e1dbabcc80474dd9a06d52169ab9264ee153
许恒 <xuheng@baidu.com>
2013-04-16 13:17:12 +0800

    [fixed] by xuheng

--- _src/plugins/defaultfilter.js
+++ _src/plugins/defaultfilter.js
@@ -1,92 +1,156 @@
 ///import core
-///commands 提供黑白名单功能
-///commandsName  filterNode
-///commandsTitle  filterNode
-/**
- * 提供黑白名单功能
- * @function
- * @name baidu.editor.execCommands
- * @param {uNode} node
- */
-UE.plugins['defaultfilter'] = function (){
+///plugin 编辑器默认的过滤转换机制
+
+UE.plugins['defaultfilter'] = function () {
     var me = this;
     //默认的过滤处理
     //进入编辑器的内容处理
     me.addInputRule(function (root) {
-        utils.each(root.getNodesByTagName('script style a img span p'), function (node) {
-            var val;
-            switch (node.tagName){
-                case 'style':
-                case 'script':
-                    node.setAttr({
-                        cdata_tag:node.tagName,
-                        cdata_data:encodeURIComponent(node.innerText() || '')
-                    });
-                    node.tagName = 'div';
-                    node.removeChild(node.firstChild());
-                    break;
-                case 'a':
-                    if (val = node.getAttr('href')) {
-                        node.setAttr('_href', val)
+        var val;
+        //进行默认的处理
+        root.traversal(function (node) {
+            if (node.type == 'element') {
+                if (me.options.autoClearEmptyNode && dtd.$inline[node.tagName] && !dtd.$empty[node.tagName] && (!node.attrs || utils.isEmptyObject(node.attrs))) {
+                    if (!node.firstChild()) node.parentNode.removeChild(node);
+                    else if (node.tagName == 'span' && (!node.attrs || utils.isEmptyObject(node.attrs))) {
+                        node.parentNode.removeChild(node, true)
                     }
-                    break;
-                case 'img':
-                    //todo base64暂时去掉，后边做远程图片上传后，干掉这个
-                    if(val = node.getAttr('src')){
-                        if(/^data:/.test(val)){
-                            node.parentNode.removeChild(node);
-                            break;
+                    return;
+                }
+                switch (node.tagName) {
+                    case 'style':
+                    case 'script':
+                        node.setAttr({
+                            cdata_tag: node.tagName,
+                            cdata_data: encodeURIComponent(node.innerText() || '')
+                        });
+                        node.tagName = 'div';
+                        node.removeChild(node.firstChild());
+                        break;
+                    case 'a':
+                        if (val = node.getAttr('href')) {
+                            node.setAttr('_href', val)
                         }
-                    }
-                    node.setAttr('_src',node.getAttr('src'));
-                    break;
-                case 'span':
-                    if(browser.webkit && (val = node.getStyle('white-space'))){
-                        if(val == 'nowrap'){
-                            node.setStyle('white-space','')
+                        break;
+                    case 'img':
+                        //todo base64暂时去掉，后边做远程图片上传后，干掉这个
+                        if (val = node.getAttr('src')) {
+                            if (/^data:/.test(val)) {
+                                node.parentNode.removeChild(node);
+                                break;
+                            }
                         }
-                    }
-                    break;
-                case 'p':
-                    if(val = node.getAttr('align')){
-                        node.setAttr('align');
-                        node.setStyle('text-align',val)
-                    }
-            }
+                        node.setAttr('_src', node.getAttr('src'));
+                        break;
+                    case 'span':
+                        if (browser.webkit && (val = node.getStyle('white-space'))) {
+                            if (/nowrap|normal/.test(val)) {
+                                node.setStyle('white-space', '');
+                                if (me.options.autoClearEmptyNode && utils.isEmptyObject(node.attrs)) {
+                                    node.parentNode.removeChild(node, true)
+                                }
+                            }
+                        }
+                        break;
+                    case 'p':
+                        if (val = node.getAttr('align')) {
+                            node.setAttr('align');
+                            node.setStyle('text-align', val)
+                        }
+                        var cssStyle = node.getAttr('style');
+                        if (cssStyle) {
+                            cssStyle = cssStyle.replace(/(margin|padding)[^;]+/g, '');
+                            node.setAttr('style', cssStyle)
 
-        });
+                        }
+                        if (!node.firstChild()) {
+                            node.innerHTML(UE.browser.ie ? '&nbsp;' : '<br>')
+                        }
+                        break;
+                    case 'div':
+                        var tmpNode, p = UE.uNode.createElement('p');
+                        while (tmpNode = node.firstChild()) {
+                            if (tmpNode.type == 'text' || !UE.dom.dtd.$block[tmpNode.tagName]) {
+                                p.appendChild(tmpNode);
+                            } else {
+                                if (p.firstChild()) {
+                                    node.parentNode.insertBefore(p, node);
+                                    p = UE.uNode.createElement('p');
+                                } else {
+                                    node.parentNode.insertBefore(tmpNode, node);
+                                }
+                            }
+                        }
+                        if (p.firstChild()) {
+                            node.parentNode.insertBefore(p, node);
+                        }
+                        node.parentNode.removeChild(node);
+                        break;
+                    case 'dl':
+                        node.tagName = 'ul';
+                        break;
+                    case 'dt':
+                    case 'dd':
+                        node.tagName = 'li';
+                        break;
+                    case 'li':
+                        var className = node.getAttr('class');
+                        if (!className || !/list\-/.test(className)) {
+                            node.setAttr()
+                        }
+                        var tmpNodes = node.getNodesByTagName('ol ul');
+                        UE.utils.each(tmpNodes, function (n) {
+                            node.parentNode.insertAfter(n, node);
+                        });
+                }
+
+            }
+        })
 
     });
 
     //从编辑器出去的内容处理
     me.addOutputRule(function (root) {
-        utils.each(root.getNodesByTagName('div a img'), function (node) {
-            var val;
-            switch (node.tagName){
-                case 'div':
-                    if (val = node.getAttr('cdata_tag')) {
-                        node.tagName = val;
-                        node.appendChild(UE.uNode.createText(node.getAttr('cdata_data')));
-                        node.setAttr({cdata_tag:'', cdata_data:''});
-                    }
-                    break;
-                case 'a':
-                    if (val = node.getAttr('_href')) {
-                        node.setAttr({
-                            'href':val,
-                            '_href':''
-                        })
-                    }
-                    break;
-                case 'img':
-                    if(val = node.getAttr('_src')){
-                        node.setAttr({
-                            'src':node.getAttr('_src'),
-                            '_src':''
-                        })
-                    }
 
+        var val;
+        root.traversal(function (node) {
+            if (node.type == 'element') {
+
+                if (me.options.autoClearEmptyNode && dtd.$inline[node.tagName] && !dtd.$empty[node.tagName] && (!node.attrs || utils.isEmptyObject(node.attrs))) {
+                    if (!node.firstChild()) node.parentNode.removeChild(node);
+                    else if (node.tagName == 'span' && (!node.attrs || utils.isEmptyObject(node.attrs))) {
+                        node.parentNode.removeChild(node, true)
+                    }
+                    return;
+                }
+                switch (node.tagName) {
+                    case 'div':
+                        if (val = node.getAttr('cdata_tag')) {
+                            node.tagName = val;
+                            node.appendChild(UE.uNode.createText(node.getAttr('cdata_data')));
+                            node.setAttr({cdata_tag: '', cdata_data: ''});
+                        }
+                        break;
+                    case 'a':
+                        if (val = node.getAttr('_href')) {
+                            node.setAttr({
+                                'href': val,
+                                '_href': ''
+                            })
+                        }
+                        break;
+                    case 'img':
+                        if (val = node.getAttr('_src')) {
+                            node.setAttr({
+                                'src': node.getAttr('_src'),
+                                '_src': ''
+                            })
+                        }
+                }
             }
-        });
+
+        })
+
+
     });
 };
