0fc727e54d744f57a98b71dc30bc7173bc50cd2b
许恒 <xuheng@baidu.com>
2013-05-06 15:34:39 +0800

    [fixed] by xuheng

--- _src/plugins/_autofloat.js
+++ _src/plugins/_autofloat.js
@@ -76,7 +76,8 @@ UE.plugins['autofloat'] = function() {
 
     function updateFloating(){
         var rect3 = getPosition(me.container);
-        if (rect3.top < 0 && rect3.bottom - toolbarBox.offsetHeight > me.options.toolbarTopOffset) {
+        var offset= me.options.toolbarTopOffset||0;
+        if (rect3.top < 0 && rect3.bottom - toolbarBox.offsetHeight > offset) {
             setFloating();
         }else{
             unsetFloating();
